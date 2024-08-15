60d99b7b89cc84ba1bc1fe090f3a7958d1abffcf
JacobKleim <yakovkleimenov@yandex.ru>
2024-08-15 15:50:19 +0400

    Improved code cleanliness. Small fix

--- game.py
+++ game.py
@@ -9,29 +9,30 @@ from animations.fire import fire
 from read_frames import read_frames
 
 TIC_TIMEOUT = 0.1
+BORDER_WIDTH = 1
+STARTING_ROW_COLUMN = 2
 
 
 async def animate_spaceship(canvas, start_row, start_column, frames):
-    frame_cycle = itertools.cycle(frames)
-    while True:
-        for frame in frame_cycle:
-            rows_direction, columns_direction, _ = read_controls(canvas)
-            frame_height, frame_width = get_frame_size(frame)
+    doubled_frame_cycle = itertools.cycle(
+        frame for frame in frames for _ in range(2)
+        )
+    for frame in doubled_frame_cycle:
+        rows_direction, columns_direction, _ = read_controls(canvas)
+        frame_height, frame_width = get_frame_size(frame)
 
-            start_row = min(max(1, start_row + rows_direction),
-                            canvas.getmaxyx()[0] - frame_height - 1)
+        start_row = min(max(1, start_row + rows_direction),
+                        canvas.getmaxyx()[0] - frame_height - BORDER_WIDTH)
 
-            start_column = min(max(1, start_column + columns_direction),
-                               canvas.getmaxyx()[1] - frame_width - 1)
+        start_column = min(max(1, start_column + columns_direction),
+                           canvas.getmaxyx()[1] - frame_width - BORDER_WIDTH)
 
-            draw_frame(canvas, start_row, start_column, frame)
-            canvas.refresh()
-            await asyncio.sleep(0)
-            await asyncio.sleep(0)
-            draw_frame(canvas, start_row, start_column, frame, negative=True)
+        draw_frame(canvas, start_row, start_column, frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, start_row, start_column, frame, negative=True)
 
 
-async def blink(canvas, row, column, symbol='*'):
+async def blink(canvas, row, column, symbol='*', offset_tics=0):
 
     curses.start_color()
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
@@ -43,7 +44,7 @@ async def blink(canvas, row, column, symbol='*'):
         for _ in range(20):
             await asyncio.sleep(0)
 
-        for _ in range(randint(0, 10)):
+        for _ in range(offset_tics):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
@@ -65,13 +66,16 @@ def draw(canvas):
     canvas.border()
 
     rows, columns = curses.window.getmaxyx(canvas)
-    max_row, max_column = rows - 2, columns - 2
+    max_row, max_column = rows - STARTING_ROW_COLUMN, columns - STARTING_ROW_COLUMN
 
     frames = read_frames('animations/rocket_frames')
 
     coroutines = [
-        blink(canvas, randint(2, max_row), randint(2, max_column),
-              choice(['*', '+', ':'])) for _ in range(40)
+        blink(canvas,
+              randint(STARTING_ROW_COLUMN, max_row),
+              randint(STARTING_ROW_COLUMN, max_column),
+              choice(['*', '+', ':']),
+              offset_tics=randint(0, 10)) for _ in range(40)
     ]
 
     fire_coroutine = fire(
@@ -82,13 +86,13 @@ def draw(canvas):
     )
     coroutines.append(fire_coroutine)
 
-    rocket_corutine = animate_spaceship(
+    rocket_coroutine = animate_spaceship(
         canvas,
-        rows / 2 - 1,
-        columns / 2 - 1,
+        rows / 2 - STARTING_ROW_COLUMN,
+        columns / 2 - STARTING_ROW_COLUMN,
         frames
     )
-    coroutines.append(rocket_corutine)
+    coroutines.append(rocket_coroutine)
 
     while True:
         for coroutine in coroutines.copy():
